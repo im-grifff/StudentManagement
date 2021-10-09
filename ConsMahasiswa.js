86abc9eb115ce97e6cd2c962766ade747dcd73f9
Griffin Mumu <griffinmumu02@gmail.com>
2021-10-10 00:14:15 +0800

    Update Filter Button

--- ConsMahasiswa.js
+++ ConsMahasiswa.js
@@ -147,7 +147,27 @@ function filter() {
     tr = table.getElementsByTagName("tr");
 
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByClassName("fakultasFilter")[0];
+        td = tr[i].getElementsByClassName("fakultas")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText || td.value;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function filter2() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("filterProdi");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("TabelStudents");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByClassName("prodi")[0];
         if (td) {
             txtValue = td.textContent || td.innerText || td.value;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
